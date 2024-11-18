b2e9423328771d784d70c3456c31074ff02bf3ef
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2024-11-18 19:20:58 +0330

    Update starrating.jsx

--- src/starrating.jsx
+++ src/starrating.jsx
@@ -1,57 +1,86 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-const componentstyle = {
+
+const containerStyle = {
   display: "flex",
   alignItems: "center",
   gap: "16px",
 };
 
-const containerstyle = {
+const starContainerStyle = {
   display: "flex",
 };
 
-const StarRating = ({ maxRating = 5, color = "#fcc419", size = 48 }) => {
-  const [rate, setRate] = useState(0);
-  const [temprating, setTemprating] = useState(0);
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
 
-  const textstyle = {
+function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
+
+  const textStyle = {
     lineHeight: "1",
     margin: "0",
     color,
-    fontsize: `${size / 1.5}px`,
+    fontSize: `${size / 1.5}px`,
   };
 
   return (
-    <div style={componentstyle}>
-      <div style={containerstyle}>
+    <div style={containerStyle} className={className}>
+      <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => setRate(i + 1)}
-            onHoverIn={() => setTemprating(i + 1)}
-            onHoverOut={() => setTemprating(0)}
-            const
-            full={temprating ? temprating >= i + 1 : rate >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
             color={color}
             size={size}
           />
         ))}
       </div>
-      <p style={textstyle}>{temprating || rate || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
-};
+}
 
-const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
-  const starstyle = {
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+  const starStyle = {
     width: `${size}px`,
     height: `${size}px`,
     display: "block",
+    cursor: "pointer",
   };
+
   return (
     <span
       role="button"
-      style={starstyle}
+      style={starStyle}
       onClick={onRate}
       onMouseEnter={onHoverIn}
       onMouseLeave={onHoverOut}
@@ -82,22 +111,12 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
       )}
     </span>
   );
-};
+}
 
-StarRating.propTypes = {
-  maxRating: PropTypes.number,
-  defaultRating: PropTypes.number,
-  color: PropTypes.string,
-  size: PropTypes.number,
-  messages: PropTypes.array,
-  className: PropTypes.string,
-  onSetRating: PropTypes.func,
-};
+/*
+FULL STAR
 
-export default StarRating;
-
-{
-  /* <svg
+<svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 20 20"
   fill="#000"
@@ -111,17 +130,7 @@ export default StarRating;
 
 EMPTY STAR
 
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg> */
-}
+
+
+*/
+export default StarRating;
