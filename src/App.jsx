b5e7aee7318990dc8de4a0e4360fa58265308c6c
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2024-11-23 20:23:59 +0330

    add Enterkeydown eventListener with useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 import StarRating from "./starrating.jsx";
 const tempMovieData = [
   {
@@ -342,6 +342,24 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+   const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
   return (
     <input
       className="search"
@@ -349,6 +367,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
